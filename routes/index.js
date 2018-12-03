3b0c65507ac5e651b31ea41a8ec86ecc4e1773b1
Yolo <kuroikenshi@sina.com>
2018-12-03 23:23:55 +0800

    首次提交

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  console.log('>>>', req)
+  res.json({
+  	status: 200,
+  	msg: '登录成功',
+  	data: {
+  	  username: 'admin'
+  	}
+  });
+});
+
+module.exports = router;
