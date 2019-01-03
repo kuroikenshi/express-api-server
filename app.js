32800e48dd4035ce9c917365d2eb71d052cbabeb
yolo <kuroikenshi@sina.com>
2019-01-03 20:27:40 +0800

    有bug，临时保存

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ccRouter = require('./routes/cc');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/cc', ccRouter);
 
 module.exports = app;
